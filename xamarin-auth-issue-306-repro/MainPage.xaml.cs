04a574c4c4c59ae9076d38583e42cbba54a3fb45
Ken Sykora <ksykora@ostusa.com>
2018-09-25 12:58:59 -0500

    Init

---
+++ xamarin-auth-issue-306-repro/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarin_auth_issue_306_repro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
